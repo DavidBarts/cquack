4ec314e616871ac732cebee0e2c53eaa62c2c0c3
David Barts <david.w.barts@gmail.com>
2024-07-10 11:24:56 -0700

    Remove deadwood.

--- error.c
+++ error.c
@@ -4,7 +4,6 @@
 #include "must.h"
 
 static char *myname = NULL;
-static const size_t VFORMAT_LEN = 128;
 static const char *DELIM = ": ";
 static const char *TERM = "\n";
 
@@ -35,27 +34,4 @@ void setmyname(const char *arg0) {
 
 const char *getmyname(void) {
     return myname;
-}
-
-char *vformat(const char *format, va_list ap) {
-    char *buf = (char *) must_malloc(VFORMAT_LEN);
-    va_list ap2;
-
-    va_copy(ap2, ap);
-    int status = vsnprintf(buf, VFORMAT_LEN, format, ap2);
-    va_end(ap2);
-    if (status < 0) {
-        buf[0] = '\0';  /* mangled format, return empty string */
-    }
-
-    if (status >= VFORMAT_LEN) {
-        size_t bufsize = status + 1;
-        buf = (char *) must_realloc(buf, bufsize);
-        status = vsnprintf(buf, bufsize, format, ap);
-        if (status < 0 || status >= bufsize) {
-            buf[0] = '\0';  /* two strikes and yer out */
-        }
-    }
-
-    return buf;
 }
